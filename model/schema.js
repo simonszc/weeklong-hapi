04a66bb9c5b1b923a9eae7d78c5a8a234ed31125
Stephen Schroeder <schrode50@gmail.com>
2016-05-29 13:38:32 -0700

    2 tests working

--- model/schema.js
+++ model/schema.js
@@ -6,7 +6,8 @@ const Kitty = new mongoose.Schema({
   name: {type: String, unique: true},
   breed: String,
   color: String,
-  gender: String
+  gender: String,
+  quote: String
 });
 
 module.exports = mongoose.model('cat', Kitty)
