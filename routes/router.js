e21f27069b8a7af91d789c03ff57059453d0ec8c
Stephen Schroeder <schrode50@gmail.com>
2016-05-29 15:21:53 -0700

    cleaning up code

--- routes/router.js
+++ routes/router.js
@@ -1,78 +1,76 @@
-//router
-'use strict'
+'use strict';
 
-const Kitty = require(__dirname + '/../model/schema.js')
+const Kitty = require(__dirname + '/../model/schema.js');
 
 module.exports = [{
-  method:'GET',
+  method: 'GET',
   path: '/cat',
-  handler: function(req,res) {
-    Kitty.find({}, (err,data) => {
-      if (err)  {
-        console.log(err)
-        return res('Error on Get')
+  handler: function(req, res) {
+    Kitty.find({}, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res('Error on Get');
       }
       return res({
         statusCode: 200,
         message: 'All the Kitties are Here',
         data: data
-      })
-    })
+      });
+    });
   }
-},{
-  method:'POST',
+}, {
+  method: 'POST',
   path: '/cat',
-  handler: function(req,res) {
+  handler: function(req, res) {
     let newKitty = new Kitty(req.payload);
-    newKitty.save((err,data) => {
-      if(err) {
-        console.log(err)
-        return res('Error on Post')
+    newKitty.save((err, data) => {
+      if (err) {
+        console.log(err);
+        return res('Error on Post');
       }
       return res({
-        statusCode:200,
+        statusCode: 200,
         message: 'Created a new kitty',
         data: data
-      })
-    })
+      });
+    });
   }
 }, {
   method: 'PUT',
   path: '/cat/{catId}',
   handler: function(req, res) {
-  Kitty.findOne({
-    _id: req.params.catId
-  }, (err, cat) => {
-    if (!err) {
-      const name = cat.name;
-      cat = req.payload;
-      Kitty.update({
-        _id: req.params.catId
-      }, cat, (err) => {
+    Kitty.findOne({
+      _id: req.params.catId
+    }, (err, cat) => {
+      if (!err) {
+        // const name = cat.name;
+        cat = req.payload;
+        Kitty.update({
+          _id: req.params.catId
+        }, cat, (err) => {
           if (!err) {
-            return res('They must have nine lives!');
-      } else {
-        console.log(err);
-        return res('Error on put');
+            res('They must have nine lives!');
+          } else {
+            return res('Error on put');
+          }
+        });
       }
-    })
-}
 
-})
-}
+    });
+  }
 }, {
-  method:'DELETE',
-  path:'/cat/{catId}',
-  handler: function(req,res) {
+  method: 'DELETE',
+  path: '/cat/{catId}',
+  handler: function(req, res) {
     Kitty.remove({
       _id: req.params.catId
     }, (err) => {
-      if(err) {
+      if (err) {
         console.log(err);
         return res('Error on Delete');
       }
-      return res('Poor kitty has run out of nine lives')
-    })
+      return res('Poor kitty has run out of nine lives');
+    });
   }
 
-}]
+}];
